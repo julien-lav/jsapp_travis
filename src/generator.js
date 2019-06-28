7b756781aab8ff73ef0defed6c7d57cfad0ca58d
julien-lav <julien.laville@hotmail.fr>
2019-06-28 11:37:21 +0200

    test travis

--- src/generator.js
+++ src/generator.js
@@ -1,9 +1,13 @@
 function createBitcoin() {
-    console.log('Loading ...')
+    console.log('Loading ...');
 
     setTimeout(() => console.log('Created !'), 1000);
 
-    let varToRemove = true;
+    let varToUse = true;
+}
+
+function use() {
+	return varToUse;
 }
 
 module.exports = {
